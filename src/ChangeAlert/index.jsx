62f5dd148db44d221799cfae7dfda9d137bbc52c
David Gaspar <davsangas@hotmail.com>
2023-02-08 09:58:17 -0600

    Styles Alert

--- src/ChangeAlert/index.jsx
+++ src/ChangeAlert/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { withStorageListener } from './withStorageListener';
+import './styles.css'
 
 function ChangeAlert({ show, toggleShow }) {
     if (show) {
         return (
-        <div>
-            <p>Hay cambios en la aplicación</p>
-            <button onClick={() => toggleShow(false)}>Cerrar</button>
+        <div className='ChangeAlert-bg'>
+            <div className='ChangeAlert-container'>
+            <p>It looks like you made a modification in another browser window</p>
+            <p>Do you want synchronize them?</p>
+            <button className='TodoForm-button TodoForm-button--add' onClick={() => toggleShow(false)}>Yes</button>
+            </div>
         </div>
         )
     } else {
